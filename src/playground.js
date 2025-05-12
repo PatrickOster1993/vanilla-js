ea8b0073f30dc896df7167beadbca18356a21c4a
Patrick Oster <p.oster9330@googlemail.com>
2025-05-12 12:49:54 +0200

    String Manipulation, Array Methoden, Objects, Functions, ternärer operator

--- src/playground.js
+++ src/playground.js
@@ -134,6 +134,14 @@ switch (a) {
     console.log("a ist", a)
 }
 
+//// Ternärer Operator
+
+a = 4
+let name = a > 5 ? "Patrick" : "Nicht-Patrick"
+
+console.log("### Ternärer Operator ###")
+console.log(name)
+
 // Scope ohne "Syntax-Konstrukt":
 {
   let hallo = "hallo"
@@ -164,3 +172,102 @@ console.log("FOR")
 for (n = 5; n > 0; n--) {
   console.log("n =", n)
 }
+
+// Arrays
+let array1 = [1, 6, 7, 9, 3, 3]
+let array2 = [3.4, "hallo", true, undefined] // JS nicht so streng wie statisch typisierte Sprachen (ähnlich wie bei Python)
+let array3 = [5, () => console.log("hallo")]
+
+//// for of
+for (zahl of array3) {
+  console.log(typeof zahl)
+}
+
+//// for in
+for (index in array3) {
+  console.log(typeof index)
+}
+
+//// Wichtige Array-Methoden:
+array1.push(100) // 100 am Ende des Arrays einfügen
+console.log(array1)
+
+array1.pop() // Das letzte Element eines Arrays entfernen
+console.log(array1)
+
+array1.shift() // Das erste Element eines Arrays entfernen!
+console.log(array1)
+
+array1.unshift(6000)
+console.log(array1)
+
+array1.reverse()
+console.log(array1)
+
+// Funktionen
+
+//// klassisch
+function myFunction(x, y) {
+  return x + y
+}
+
+console.log("klassische Funktion:", myFunction(3, 5))
+
+myArrayFunction = (x, y) => console.log("Arrow-Funktion:", x + y)
+
+// function matheStuff(operation) {
+//   return operation
+// }
+
+// addiere = (a, b) => {
+//   return a + b
+// }
+// matheStuff(addiere(a, b))
+// matheStuff(() => console.log("Heute keine Operation!"))
+
+// Objects
+
+let person = {
+  name: {
+    vorname: "Patrick",
+    nachname: "Oster",
+  },
+  alter: {
+    echt: 31,
+    gelogen: 25,
+  },
+  lieblingsessen: "Pad Thai",
+}
+
+console.log(typeof person)
+console.log(person)
+console.log("Mein Lieblingsessen:", person.lieblingsessen)
+console.log("Mein echtes Alter:", person.alter.echt)
+
+// Ein paar String-Manipulationen / -Methoden:
+
+let myString = "       Hallo DAA!"
+
+console.log(myString.toLowerCase()) // alles in Kleinbuchstaben
+console.log(myString.toUpperCase()) // alles in Großbuchstaben
+
+let trimString = myString.trim() // Use-Case: Leerzeichen zu Beginn entfernen (= trimmen)
+
+console.log(trimString)
+
+console.log(myString.includes("DAA")) // Enthält String übergebenen Substring?
+
+let subString = trimString.slice(0, 3) // Subsring von Beginn- und End-Index erstellen!
+console.log(subString)
+
+function myReplacementFunc(oldSub, newSub) {
+  if (trimString.includes(oldSub)) {
+    console.log(trimString.replace(oldSub, newSub))
+  } else {
+    console.log(trimString)
+  }
+}
+
+myReplacementFunc("DAA", "Welt")
+myReplacementFunc("DOO", "Welt")
+myReplacementFunc("Hallo", "Servus")
